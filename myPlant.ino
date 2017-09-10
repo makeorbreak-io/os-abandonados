d936964a076c2c5bd4b00b0b53189715b7209ec6
João Domingues <joaopldomingues@gmail.com>
2017-09-10 11:11:25 +0100

    Add files via upload

--- myPlant.ino
+++ myPlant.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+#include <rgb_lcd.h>
 
 /**
    Light sensor, connected to the Analog Pin #2,, ranging from 0 to 1023
@@ -58,10 +60,11 @@ int minimumDelayBetweenBombActivationTimer = 0;
    Default minimum required time between the water bomb activations
 */
 int minimumDelayBetweenBombActivationTimerDefault = 1200; // 1200 = 1 min
+
 /**
    Timer to update the LCD
 */
-int lcdUpdateTimer = 20;
+int lcdUpdateTimer = 10;
 
 /**
    humidityValue converted to %
@@ -106,18 +109,182 @@ int humidityAverageIndex = 0;
 /**
    Standard time it takes for the lcd to update (in # of 50ms cycles)
 */
-int lcdStandardTimer = 12000; // 12000 = 10 mins
+int lcdStandardTimer = 10; // 10 = 0.5 sec
 
 /**
-   Minimum humidity desired for the plant (358 = ~65%)
+   Minimum humidity desired for the plant (310= ~70%)
 */
-int minimumHumidityDesired = 358;
+int minimumHumidityDesired = 310;
 
+/**
+  The current menu index
+*/
+int menuIndex = 0;
+
+/*
+   Number of cycles representing the countdown for the water bomb to activate
+*/
+int manualModeCurrentIndex = 0;
+
+/*
+   Number cycles representing the time it takes for the water bomb to activate
+*/
+int manualModeDefaultIndex = 100;
+
+/**
+   Declaring the lcd
+*/
+rgb_lcd lcd;
+
+/**
+   The background colours of the lcd
+*/
+int colorR;
+int colorG;
+int colorB;
 
 void setup() {
   pinMode(relePin, OUTPUT);
+  pinMode(lightSensorPin, INPUT);
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  lcd.setRGB(0, 255, 0);
+
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("                 M");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("                My");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 13);
+  lcd.print("               MyP");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("              MyPl");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("             MyPla");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("            MyPlan");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("           MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("          MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("         MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("        MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("       MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("      MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 9);
+  lcd.print("     MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("    MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("   MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("  MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print(" MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("MyPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("yPlant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("Plant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("Lant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("ant");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("nt");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("t");
+  delay(250);
+
+  lcd.newclear(10);
+  lcd.setCursor(0, 0);
+  lcd.print("");
+  delay(250);
+
+
+
+  lcd.clear();
+  lcd.setCursor(5, 0);
+  lcd.print("MyPlant");
+  delay(1000);
+
 }
 
 void loop() {
@@ -130,29 +297,46 @@ void loop() {
   humidityPercentage = 1023 - humidityValue;
   humidityPercentage = humidityPercentage * 100;
   humidityPercentage = humidityPercentage / 1023;
-  lightPercentage = lightValue * 100;
-  lightPercentage = lightPercentage / 1023;
 
-  if (buttonState == 0) {
+  if (buttonState == 0 && menuIndex != 4) {
 
     checkIfBombNeedsToBeActivated();
 
-    lcdUpdate();
+    // lcdUpdate();
+
 
     deactivateBomb();
 
-    delay(defaultDelay);
 
-    timerUpdate();
+    //delay(defaultDelay);
 
-  } else { // Manual water bomb activation
+    //timerUpdate();
 
-    manualMode();
+  } else if (buttonState == 1) { // On button press
 
+    if (menuIndex < 3) {
+      menuIndex = menuIndex + 1;
+    } else if (menuIndex == 3) {
+      menuIndex = menuIndex + 1;
+      manualModeCurrentIndex = 0;
+    } else {
+      deactivateBomb();
+      menuIndex = 0;
+    }
+    delay(500);
   }
 
   calculateAverageHumidity();
 
+  if (menuIndex == 4) {
+    manualModeCurrentIndex = manualModeCurrentIndex + 1;
+  }
+
+  lcdUpdate();
+
+  delay(defaultDelay);
+
+  timerUpdate();
 }
 
 /**
@@ -185,14 +369,116 @@ void timerUpdate() {
 }
 
 /**
-   Updates the LCD every 5 seconds (100 50ms cycles)
+   Updates the LCD every 0.5 seconds (10 50ms cycles)
 */
 void lcdUpdate() {
   if (lcdUpdateTimer == 0) {
-    double totalTime = cyclesAverage * 50;
+
+    if (menuIndex == 0) {
+      lcdHumidityMenu();
+    } else if (menuIndex == 1) {
+      lcdLightMenu();
+    } else if (menuIndex == 2) {
+      lcdLastIrrigationMenu();
+    } else if (menuIndex == 3) {
+      lcdAverageHumidityMenu();
+    } else if (menuIndex == 4) {
+      lcdManualMode();
+    }
 
     lcdUpdateTimer = lcdStandardTimer;
+
   }
+
+}
+
+/*
+  LCD's Humidity menu
+*/
+void lcdHumidityMenu() {
+
+  if (humidityPercentage > 70) {
+    colorR = 0;
+    colorG = 255;
+    colorB = 0;
+  } else {
+    colorR = 255;
+    colorG = 0;
+    colorB = 0;
+  }
+
+  lcdMessageAndColorHumidity("Humidity:", 4, humidityPercentage);
+}
+
+/**
+    Changes the lcd message and color according to the plants humidity percentage
+*/
+void lcdMessageAndColorHumidity(String aspect, int start, double percentage) {
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.clear();
+  lcd.setCursor(start, 0);
+  lcd.print(aspect);
+  lcd.setCursor(5, 1);
+  lcd.print(percentage);
+  lcd.setCursor(12, 1);
+  lcd.print("%");
+}
+
+/**
+    Changes the lcd message and color according to the plants light percentage
+*/
+void lcdMessageAndColorLight(String aspect, int start, String state) {
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.clear();
+  lcd.setCursor(start, 0);
+  lcd.print(aspect);
+  lcd.setCursor(5, 1);
+  lcd.print(state);
+}
+
+/*
+  LCD's light menu
+*/
+void lcdLightMenu() {
+  String state;
+  if (lightValue > 600) {
+    colorR = 0;
+    colorG = 255;
+    colorB = 0;
+    state = "Good";
+  } else if (lightValue <= 600 && lightValue > 300) {
+    colorR = 255;
+    colorG = 255;
+    colorB = 0;
+    state = "Medium";
+  } else if (lightValue <= 300) {
+    colorR = 255;
+    colorG = 0;
+    colorB = 0;
+    state = "Low";
+  }
+  Serial.println(lightValue);
+  lcdMessageAndColorLight("Light:", 5, state);
+
+}
+
+/*
+  LCD's time since last irrigation menu
+*/
+void lcdLastIrrigationMenu() {
+  double totalTime = cyclesAverage * 0.050;
+  lcd.setRGB(54, 81, 94);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Last Irrigation");
+  lcd.setCursor(3, 1);
+  lcd.print((int) totalTime / 60);
+  lcd.setCursor(5, 1);
+  lcd.print("min");
+  lcd.setCursor(9, 1);
+  lcd.print((int)totalTime % 60);
+  lcd.setCursor(11, 1);
+  lcd.print("sec");
 }
 
 /**
@@ -202,6 +488,7 @@ void deactivateBomb() {
   if ((waterBombActivationTimer == 0) && (releaux == 1)) {
     digitalWrite(relePin, LOW);
     releaux = 0;
+    minimumDelayBetweenBombActivationTimer = minimumDelayBetweenBombActivationTimerDefault - 1;
   }
 }
 
@@ -209,8 +496,6 @@ void deactivateBomb() {
    Activates the water bomb
 */
 void activateBomb() {
-  double totalTime = cyclesAverage * 50;
-
   digitalWrite(relePin, HIGH);
   releaux = 1;
   calculateAverageTime();
@@ -228,9 +513,57 @@ void calculateAverageTime() {
 /**
    Manual mode
 */
-void manualMode() {
-  activateBomb();
-  cyclesAverage = 0;
+void lcdManualMode() {
+  // manualModeCurrentIndex = manualModeCurrentIndex + 1;
+
+  /*lcd.setRGB(58, 44, 86);
+    lcd.clear();
+    lcd.setCursor(2, 0);
+    lcd.print("Manual Mode");*/
+
+  countDown();
+
+  if (manualModeCurrentIndex >= manualModeDefaultIndex) {
+    activateBomb();
+    cyclesAverage = 0;
+    waterBombActivationTimer = 0;
+
+    lcd.setRGB(100, 65, 0);
+    lcd.clear();
+    lcd.setCursor(2, 0);
+    lcd.print("Manual Mode");
+    lcd.setCursor(2, 1);
+    lcd.print("Press to Stop");
+  }
+}
+
+/*
+   Represents the countdown for the manual mode to activate
+*/
+void countDown() {
+  if (manualModeCurrentIndex <= 20) {
+    repaintManualModeCountDown(5);
+  } else if (manualModeCurrentIndex <= 40) {
+    repaintManualModeCountDown(4);
+  } else if (manualModeCurrentIndex <= 60) {
+    repaintManualModeCountDown(3);
+  } else if (manualModeCurrentIndex <= 80) {
+    repaintManualModeCountDown(2);
+  } else {
+    repaintManualModeCountDown(1);
+  }
+}
+
+/*
+   Repaints the lcd, representing the countdown for the manual mode to begin
+*/
+void repaintManualModeCountDown(int index) {
+  lcd.setRGB(58, 44, 86);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Manual Mode");
+  lcd.setCursor(8, 1);
+  lcd.print(index);
 }
 
 /**
@@ -241,4 +574,17 @@ void calculateAverageHumidity() {
   humidityGlobalAverage = (humidityGlobalAverage * humidityAverageIndex + humidityPercentage) / (humidityAverageIndex + 1);
 }
 
+/*
+   Menu showing the average humidity in the plant
+*/
+void lcdAverageHumidityMenu() {
+  lcd.setRGB(255, 182, 193);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Average Hum%");
+  lcd.setCursor(5, 1);
+  lcd.print(humidityGlobalAverage);
+  lcd.setCursor(12, 1);
+  lcd.print("%");
+}
 
